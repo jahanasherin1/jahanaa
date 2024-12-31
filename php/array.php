b187d6d7a1241a8c9189e33c9d5c29698f937a4c
jahanasherin1 <jahanasherin2311@gmail.com>
2024-12-31 14:05:09 +0530

    Add files via upload

---
+++ php/array.php
@@ -0,0 +1,11 @@
+<?php
+$a2=array('h1','h3','h4');
+echo "<br>og array:";
+print_r($a2);
+echo "<br>Sorts the array in order<br>";
+arsort($a2);
+print_r($a2);
+echo "<br>Sorts the array again in reverse order";
+arsort($a2);
+print_r($a2);
+?>
